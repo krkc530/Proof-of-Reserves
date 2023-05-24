9dd8e8824d8681ae55d389e83204bc19c22e5d47
kim <kyoungbin1996@gmail.com>
2023-05-24 10:38:22 +0900

    groth16 verify, user upload contract 구현완료 🫠

--- contract/contracts/proofOfReserves.sol
+++ contract/contracts/proofOfReserves.sol
@@ -5,7 +5,17 @@ import "./ccGroth16VerifyBn128.sol";
 
 contract ProofOfReservesContract {
 
+    // G1 Point (x, y)
+    struct Commitment {
+        uint256 px;
+        uint256 py;
+    }
+
     uint256[] private verifyingKey;
+    
+    Commitment[] commitArray;
+
+    uint256 private commitCnt=0;
 
     constructor(
         uint256[] memory _vk
@@ -27,10 +37,36 @@ contract ProofOfReservesContract {
             input_proof[i] = proof[i];
         }
         require(ccGroth16BN128._verify(verifyingKey, input_proof), 'verify fail');
+
+
         return true;
     }
 
-    function get_vk()public view returns(uint256[] memory){
+    function upload_commitment(
+        uint256[] memory proof
+    ) 
+        public
+        payable
+        returns (bool)
+    {   
+        require(ccGroth16BN128._verify(verifyingKey, proof), 'verify fail');
+
+        Commitment memory cm = Commitment(proof[8], proof[9]);
+        commitArray.push(cm);
+        commitCnt++;
+
+        return true;
+    }
+
+    function get_vk() public view returns(uint256[] memory){
         return verifyingKey;
+    }
+
+    function get_all_commitments() public view returns(Commitment[] memory){
+        return commitArray;
+    }
+
+    function get_commitment_cnt() public view returns(uint256){
+        return commitCnt;
     }
 }
