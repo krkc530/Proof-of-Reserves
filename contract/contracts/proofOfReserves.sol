6fa2d2fb99ec53cff1861b77bcd15c96ce2b6272
kim <kyoungbin1996@gmail.com>
2023-05-25 14:09:57 +0900

    .

--- contract/contracts/proofOfReserves.sol
+++ contract/contracts/proofOfReserves.sol
@@ -20,15 +20,30 @@ contract ProofOfReservesContract {
         verifyingKey = _vk;
     }
 
+    // Proof :
+    //      uint256[2] A    : G_1
+    //      uint256[4] B    : G_2
+    //      uint256[2] C    : G_1
+    //      uint256[2] D    : G_1
     function upload_commitment(
         uint256[] memory proof
-    ) public payable returns (bool) {
+    ) public payable returns (uint256) {
         require(ccGroth16BN128._verify(verifyingKey, proof), "verify fail");
 
         Commitment memory cm = Commitment(proof[8], proof[9]);
         commitArray.push(cm);
-        commitCnt++;
+        return commitCnt++;
+    }
 
+    function update_commitment(
+        uint256 idx,
+        uint256[] memory proof
+    ) public payable returns (bool) {
+        require(idx < commitCnt, "Invalid index");
+        require(ccGroth16BN128._verify(verifyingKey, proof), "verify fail");
+
+        commitArray[idx].px = proof[8];
+        commitArray[idx].py = proof[9];
         return true;
     }
 
