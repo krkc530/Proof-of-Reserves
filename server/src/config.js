78f1a755e76f5e628b9d141787b5c0a286e35fb8
krkc530 <krkc530@naver.com>
2023-09-06 14:10:00 +0900

    fix: .gitignore

--- server/src/config.js
+++ server/src/config.js
@@ -2,12 +2,11 @@ export default {
   "DB": {
     "host": "127.0.0.1",
     "user": "root",
-    "database": "POR",
-    "password": "1157310"
+    "database": "POR"
   },
   "PATH": {
-    "homePath": "/Users/kim/Proof-of-Reserves/server/",
-    "proofPath": "/Users/kim/Proof-of-Reserves/server/napirs-legogroth16/json/"
+    "homePath": "/Users/freeman/dev/Proof-of-Reserves/server/",
+    "proofPath": "/Users/freeman/dev/Proof-of-Reserves/server/napirs-legogroth16/json/"
   },
   "testProvider": "http://127.0.0.1:7545",
   "EC_TYPE": "EC_ALT_BN128"
