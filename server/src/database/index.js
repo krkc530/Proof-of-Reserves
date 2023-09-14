6fc8ce6b29b2db4ea2728828e2596b39ae65d5d2
krkc530 <krkc530@naver.com>
2023-09-14 15:31:43 +0900

    fix: db returns pool

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -2,11 +2,12 @@ import mysql2 from "mysql2/promise";
 import config from "../config";
 
 // singleton instance config
-const DbInstance = await mysql2.createConnection({
+const DbInstance = mysql2.createPool({
   host: config.DB.host,
   user: config.DB.user,
   password: config.DB.password,
   database: "POR",
+  enableKeepAlive: true,
 });
 
 export default DbInstance;
